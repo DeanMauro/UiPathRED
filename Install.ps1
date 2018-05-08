6fa61f95ca2d1123258d1ba8951b2da23bfa3f7b
Dean Mauro <DeanMauro@users.noreply.github.com>
2018-05-08 11:11:21 -0400

    Simplified

---
+++ Install.ps1
@@ -0,0 +1,11 @@
+﻿# Download Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Download Node
+choco install nodejs.install
+
+# Download Git
+choco install git
+
+# Install NodeRED
+npm install
