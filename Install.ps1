f93cabd8dc2251f8f459b98823f6dd66ddd886ba
Dean Mauro <DeanMauro@users.noreply.github.com>
2018-05-08 11:57:47 -0400

    node version

--- Install.ps1
+++ Install.ps1
@@ -1,8 +1,11 @@
-﻿# Download Chocolatey
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+
+# Download Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Download Node
-choco install nodejs.install
+choco install nodejs-lts
 
 # Download Git
 choco install git
