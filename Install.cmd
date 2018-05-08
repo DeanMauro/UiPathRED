7bb7ecc099aee9e24a258b162bf945cf7490fc80
Dean Mauro <DeanMauro@users.noreply.github.com>
2018-05-08 14:24:17 -0400

    Dropped to cmd

---
+++ Install.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+REM Download Chocolatey
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+REM Download Node
+choco install -y nodejs-lts
+
+REM Download Git
+choco install -y git
+
+REM Install NodeRED
+npm install
