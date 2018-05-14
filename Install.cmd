2d1f5adf59f6a574022553b0e1b4e83f3a8fce8c
Dean Mauro <DeanMauro@users.noreply.github.com>
2018-05-14 09:45:32 -0400

    Fixed links and permissions

--- Install.cmd
+++ Install.cmd
@@ -1,10 +1,16 @@
 @echo off
 
+REM Switch to Script Dir
+cd %~dp0
+
 REM Download Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM Download Node
 choco install -y nodejs-lts
+
+REM Refresh PATH
+call refreshenv
 
 REM Download Git
 choco install -y git
